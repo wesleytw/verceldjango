37bb80393e2e45ed59195fa761f8bb9d8b13057b
wesleytw <wesliutw@gmail.com>
2022-11-18 15:27:57 +0800

    feat, style: response from outter .py file, use autopep8 formatter

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from. import views
+from . import views
 
 urlpatterns = [
-  path('', views.getData),
-  path('/res', views.resItem),
-  path('/save', views.saveItem),
+    path('', views.getData),
+    path('/res', views.resItem),
+    path('/save', views.saveItem),
+    path('/call', views.callItem),
 ]
--- api/views.py
+++ api/views.py
@@ -2,23 +2,34 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 # from base.models import Item
 from .serializers import ItemSerializer
+from verceldjango.ml import call
 
-@api_view (['GET'])
+
+@api_view(['GET'])
 def getData(req):
-  person = {'name': 'wes', 'age': 20}
-  return Response(person)
+    person = {'name': 'wes', 'age': 20}
+    return Response(person)
 
-@api_view (['POST'])
+
+@api_view(['POST'])
 def resItem(req):
-  # serializer = ItemSerializer(data=req.data)
-  # if serializer.is_valid():
-  #   serializer.save()
-  #   return Response(serializer.data)
-  return Response(req.data)
+    # serializer = ItemSerializer(data=req.data)
+    # if serializer.is_valid():
+    #   serializer.save()
+    #   return Response(serializer.data)
+    return Response(req.data)
 
-@api_view (['POST'])
+
+@api_view(['POST'])
 def saveItem(req):
-  serializer = ItemSerializer(data=req.data)
-  if serializer.is_valid():
-    serializer.save()
-    return Response(serializer.data)
+    serializer = ItemSerializer(data=req.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+
+
+@api_view(['POST'])
+def callItem(req):
+    item = call(req.data)
+    print(req,item)
+    return Response(item)
